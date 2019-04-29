b16042bafe3d82e52f0be41c3d3ff2a07ca1c852
F4MED <admin@f4med.com>
2019-04-29 21:58:03 +0300

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,10 @@
+$(".free").click(function(){
+    this.setAttribute("class","free seleced");
+});
+$(".button#paying").click(function(){
+    document.querySelector(".pay").setAttribute("class",
+        "pay c")
+})
+$(".pay>.button").click(function(){
+    
+});
